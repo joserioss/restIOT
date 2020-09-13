530bcf55036e9c994472c1f1392d1ee21381dfd2
Jose Rios <lithium@hotmail.es>
2020-09-12 22:26:07 -0300

    nivel de madures de richardson 1 en controlador dispositivo

--- src/main/java/cl/jrios/controller/DispositivoController.java
+++ src/main/java/cl/jrios/controller/DispositivoController.java
@@ -3,6 +3,8 @@ package cl.jrios.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,27 +25,33 @@ public class DispositivoController {
 	private DispositivoService service;
 
 	@GetMapping
-	public List<Dispositivo> listar() {
-		return service.listar();
+	public ResponseEntity<List<Dispositivo>> listar() {
+		List<Dispositivo> lista = service.listar();
+		return new ResponseEntity<List<Dispositivo>>(lista, HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	public Dispositivo listarPorId(@PathVariable("id") Integer id) {
-		return service.leerPorId(id);
+	public ResponseEntity<Dispositivo> listarPorId(@PathVariable("id") Integer id) {
+		Dispositivo dispo = service.leerPorId(id);
+		return new ResponseEntity<Dispositivo>(dispo, HttpStatus.OK);
 	}
 
 	@PostMapping
-	public Dispositivo registrar(@RequestBody  Dispositivo dispo) {
-		return service.registrar(dispo);
+	public ResponseEntity<Dispositivo> registrar(@RequestBody Dispositivo dispositivo) {
+		Dispositivo dispo =  service.registrar(dispositivo);
+		return new ResponseEntity<Dispositivo>(dispo, HttpStatus.CREATED);
+
 	}
-	
+
 	@PutMapping
-	public Dispositivo modificar(@RequestBody Dispositivo dispo) {
-		return service.modificar(dispo);
+	public ResponseEntity<Dispositivo> modificar(@RequestBody Dispositivo dispositivo) {
+		Dispositivo dispo =  service.modificar(dispositivo);
+		return new ResponseEntity<Dispositivo>(dispo, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public void eliminar(@PathVariable("id") Integer id) {
+	public ResponseEntity<Object> eliminar(@PathVariable("id") Integer id) {
 		service.eliminar(id);
+		return new ResponseEntity<Object>(HttpStatus.OK);
 	}
 }
